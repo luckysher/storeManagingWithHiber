b3ddd4ae34b5aeddb0dd017639496af11eb80cb6
luckysher <luckyshera95@gmail.com>
2017-11-27 17:38:53 +0530

    added constructor

--- src/main/test/storage/StorageTests.java
+++ src/main/test/storage/StorageTests.java
@@ -6,5 +6,11 @@ import utils.HibernateUtils;
 
 public class StorageTests extends TestCase{
 	
+	LogFactory logger = null;
+	//constructor
+	public StorageTests(){
+		this.logger = new LogFactory("StorageTests");
+	}
+	
 	
 }
