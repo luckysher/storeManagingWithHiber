f0beb168dcc5bc7bd83ebd87909545732cc70d44
luckysher <luckyshera95@gmail.com>
2017-11-28 18:05:02 +0530

    added util class for store manager

--- src/main/java/utils/HibernateUtils.java
+++ src/main/java/utils/HibernateUtils.java
@@ -5,4 +5,9 @@ import org.hibernate.cfg.AnnotationConfiguration;
 
 import logger.LogFactory;
 
-
+public class HibernateUtils {
+	
+	private static final SessionFactory sessionFactory;
+	public static LogFactory logger = new LogFactory();
+	
+}
