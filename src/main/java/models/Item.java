9e31e411b99cd04570acbc1d2bf743552c3dfa27
luckysher <luckyshera95@gmail.com>
2017-11-28 17:59:39 +0530

    added fields for item model

--- src/main/java/models/Item.java
+++ src/main/java/models/Item.java
@@ -3,5 +3,13 @@ package models;
 import java.util.Date;
 
 public class Item {
+	private int id;
+	private int price;
+	private String name;
+	private Date buyingdate;
+	private Boolean expire;
+	private String details;
+	
+	
 		
 }
