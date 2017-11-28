f28f0c0010e324d09c980ad7f3d6084092d93554
luckysher <luckyshera95@gmail.com>
2017-11-28 18:05:43 +0530

    added session utils code for store manage

--- src/main/java/utils/HibernateUtils.java
+++ src/main/java/utils/HibernateUtils.java
@@ -9,5 +9,15 @@ public class HibernateUtils {
 	
 	private static final SessionFactory sessionFactory;
 	public static LogFactory logger = new LogFactory();
+	static {
+		try{
+			logger.info("Initializing session factory..");
+			sessionFactory = new AnnotationConfiguration().configure("/applicationContext.xml").buildSessionFactory();
+		}catch(Throwable ex){
+			logger.warning("Got exception while creating session factory: %s", ex);
+			throw new ExceptionInInitializerError(ex);
+		}		
+	}
+	
 	
 }
