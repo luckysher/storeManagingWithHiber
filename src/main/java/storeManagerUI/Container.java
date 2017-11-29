9b312e79ef4d03e86cf765d65fbe9b487ff8b886
luckysher <luckyshera95@gmail.com>
2017-11-29 18:19:13 +0530

    added class for storage UI container

---
+++ src/main/java/storeManagerUI/Container.java
@@ -0,0 +1,13 @@
+package storeManagerUI;
+
+import java.awt.Color;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+import utils.StorageUiUtils;
+
+public class Container extends JPanel {
+
+	
+}
