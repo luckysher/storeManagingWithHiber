519f3506c6aef5468595d381f6fc11002007c882
luckysher <luckyshera95@gmail.com>
2017-11-30 18:29:51 +0530

    added imports for main file for store UI

--- src/main/java/storeManagerUI/StoreManUI.java
+++ src/main/java/storeManagerUI/StoreManUI.java
@@ -1,3 +1,16 @@
+package storeManagerUI;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import utils.StorageUiUtils;
 
 
 /*
