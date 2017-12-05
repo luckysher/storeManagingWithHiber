c43013c9785ba43d5cd7b967999b2be7fcee1670
luckysher <luckyshera95@gmail.com>
2017-12-06 00:01:47 +0530

    added method for adding component

--- src/main/java/storeManagerUI/ItemStore.java
+++ src/main/java/storeManagerUI/ItemStore.java
@@ -17,7 +17,22 @@ import javax.swing.JTextField;
 
 public class ItemStore extends JPanel {
 		
-
+	public void addComponent(GridBagLayout gbl, GridBagConstraints gbc, Component comp){	
+			gbl.setConstraints(comp, gbc);
+			add(comp);
+		}
+	
+	public void addTopHeading(GridBagLayout gbl, GridBagConstraints gbc, String heading){
+		gbc.insets = new Insets(15, 0, 5, 0);	
+		gbc.gridwidth = GridBagConstraints.REMAINDER;		
+		gbc.weighty = 0.25;
+		gbc.weightx = 0.25;		
+		gbc.fill = GridBagConstraints.BOTH;
+		JLabel formLabel = new JLabel(heading, JLabel.CENTER);
+		formLabel.setFont(new Font("SansSerif", Font.BOLD, 16));
+		addComponent(gbl, gbc, formLabel);		
+	}
+	
 	public void setLabel(GridBagLayout gbl, GridBagConstraints gbc, String lname){
 		gbc.insets = new Insets(15, 50, 0, 20);	
 		gbc.gridwidth = GridBagConstraints.RELATIVE;
