f94be6ccf043e2bd82eadc4b91600f8e2d7ef32a
luckysher <luckyshera95@gmail.com>
2017-12-05 23:38:10 +0530

    updated store manager ui

--- src/main/java/storeManagerUI/ItemStore.java
+++ src/main/java/storeManagerUI/ItemStore.java
@@ -2,59 +2,33 @@ package storeManagerUI;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class ItemStore extends JPanel {
-	
-	public void addComponent(GridBagLayout gbl, GridBagConstraints gbc, Component comp){
-		gbl.setConstraints(comp, gbc);
-		add(comp);
-	}
 	
 	public ItemStore(){		
 		GridBagLayout gbl = new GridBagLayout();
-		GridBagConstraints gbc = new GridBagConstraints();
-		this.setLayout(gbl);
+		GridBagConstraints gbc = new GridBagConstraints();		
+		this.setLayout(gbl);   
 		
-		this.setFont(new Font("SansSerif", Font.BOLD, 14));
-	
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.weightx = 0.0;
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		JLabel formLabel = new JLabel("Item store form", JLabel.CENTER);
-		formLabel.setFont(new Font("SansSerif", Font.BOLD, 16));
-		addComponent(gbl, gbc, formLabel);
-
-		gbc.weightx = 1.0;
-		gbc.gridwidth = GridBagConstraints.RELATIVE;
-		JLabel nameLabel = new JLabel("Name", JLabel.CENTER);
-		nameLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
-		addComponent(gbl, gbc, nameLabel);
-		
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		JTextField namefield = new JTextField();
-		addComponent(gbl, gbc, namefield);
-	
-		gbc.gridwidth = GridBagConstraints.RELATIVE;
-		JLabel priceLabel = new JLabel("Price", JLabel.CENTER);
-		priceLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
-		addComponent(gbl, gbc, priceLabel);
-		
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		JTextField pricefield = new JTextField();
-		//nameLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
-		addComponent(gbl, gbc, pricefield);		
-		
-		
-				
+		//set main heading
+		setToolTipText("Storage form");
+		addTopHeading(gbl, gbc, "Storage form");
 		
 	
+		
+		
+		
 	 }	
 	}
