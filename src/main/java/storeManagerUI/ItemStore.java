7492dbb86224b3fe7f89a38cb5c2760ea6521587
luckysher <luckyshera95@gmail.com>
2017-12-06 19:32:57 +0530

    updated store item ui

--- src/main/java/storeManagerUI/ItemStore.java
+++ src/main/java/storeManagerUI/ItemStore.java
@@ -1,13 +1,11 @@
 package storeManagerUI;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Rectangle;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -15,8 +13,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class ItemStore extends JPanel {
-		
+public class ItemStore extends JPanel implements ActionListener, ItemListener {
+	
 	public void addComponent(GridBagLayout gbl, GridBagConstraints gbc, Component comp){	
 			gbl.setConstraints(comp, gbc);
 			add(comp);
@@ -46,54 +44,63 @@ public class ItemStore extends JPanel {
 		addComponent(gbl, gbc, label);		
 	}
 	
-	public void setTextField(GridBagLayout gbl, GridBagConstraints gbc){
-		gbc.insets = new Insets(15, 15, 0, 200);
+	public JTextField setTextField(GridBagLayout gbl, GridBagConstraints gbc){
+		gbc.insets = new Insets(15, 15, 0, 350);
 		gbc.gridwidth = GridBagConstraints.REMAINDER;		
 		gbc.gridheight = 1;
 		gbc.weighty = 0.03;
 		gbc.weightx = 0.7;		
 		gbc.fill = GridBagConstraints.BOTH;
+	
 		JTextField tfield = new JTextField();
 		tfield.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		addComponent(gbl, gbc, tfield);				
+		addComponent(gbl, gbc, tfield);
+	
 	}
-	
-	
+		
 	public ItemStore(){		
 		GridBagLayout gbl = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();		
 		this.setLayout(gbl);   
-		
+			
 		//set main heading
 		setToolTipText("Storage form");
 		addTopHeading(gbl, gbc, "Storage form");
 		
 		// Set Name label and text field
 		setLabel(gbl, gbc, "Name");
-		setTextField(gbl, gbc);
+		this.namefield = setTextField(gbl, gbc);
 				
 		// Set Name label and text field
 		setLabel(gbl, gbc, "Price");
-		setTextField(gbl, gbc);
+		this.pricefield = setTextField(gbl, gbc);
 					
 		// Set Buying label and text field
 		setLabel(gbl, gbc, "Buying date");
-		setTextField(gbl, gbc);
+		this.buydatefield = setTextField(gbl, gbc);
 		
 		// Set Expire label and text field
-		setLabel(gbl, gbc, "Expire");
-		setTextField(gbl, gbc);
-		
+			
+		gbc.fill = GridBagConstraints.BOTH;	
+		JCheckBox expire = new JCheckBox();
+		addComponent(gbl, gbc, expire);
+			
 		// Set Details label and text field
-		setLabel(gbl, gbc, "Details");
-		setTextField(gbl, gbc);
+		gbc.insets = new Insets(15, 15, 0, 20);		
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridwidth = GridBagConstraints.RELATIVE;
+		gbc.weighty = 0.05;
+		JLabel label = new JLabel("Details", JLabel.RIGHT);
+		label.setFont(new Font("SansSerif", Font.PLAIN, 16));				
+		addComponent(gbl, gbc, label);
+	;		
 		
-		gbc.insets = new Insets(15, 700, 200, 400);
+		gbc.insets = new Insets(15, 700, 200, 500);
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.gridwidth = GridBagConstraints.CENTER;
-		gbc.weighty = 0.05;		
+		gbc.weighty = 0.04;		
 		JButton button = new JButton("Store info");
-		addComponent(gbl, gbc, button);	
+		addComponent(gbl, gbc, button);
+	}
 		
-	 }	
 	}
